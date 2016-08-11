5e27d010401691bb17276bd82bafca6bbc2da521
phoebegl <phoebegl74@163.com>
2016-08-12 00:45:04 +0800

    数据层

--- src/main/java/edu/nju/dao/mapper/UserMapper.java
+++ src/main/java/edu/nju/dao/mapper/UserMapper.java
@@ -2,16 +2,101 @@ package edu.nju.dao.mapper;
 
 import edu.nju.model.User;
 
+import java.sql.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public interface UserMapper {
-    int deleteByPrimaryKey(String login);
 
     int insert(User record);
 
-    int insertSelective(User record);
+    int insertOwn(String login,String full_name);
 
-    User selectByPrimaryKey(String login);
+    List<User> selectAll();
 
-    int updateByPrimaryKeySelective(User record);
+    User selectByLogin(String login);
 
-    int updateByPrimaryKey(User record);
+    List<User> selectAllWithPager(Map<String,Object> params);
+
+    List<User> searchUser(Map<String,Object> params);
+
+    List<LinkedHashMap> countFirst30Companys();
+
+    List<LinkedHashMap> countTypes();
+
+    List<LinkedHashMap> countCreate_Year();
+
+    List<User> selectUserByLanguage(Map<String,Object> params);
+
+    List<User> selectUserByCompany(Map<String,Object> params);
+
+    List<User> selectLan_Com(String language, String company, int pageSize, int pageOffset);
+
+    int countLanguage(String language);
+
+    int countCompany(String company);
+
+    Integer countContribute(String login);
+
+    List<String> selectContriRepo(String login);
+
+    List<String> selectCollaRepo(String login);
+
+    List<String> selectOwn(String login);
+
+    List<String> selectEmail();
+
+    List<LinkedHashMap> countEmail();
+
+    int countBlog(String blog);
+
+    List<Integer>countFollowers();
+
+    List<Integer>countFollowings();
+
+    int countFans(int first,int last);
+
+    int countFollow(int first,int last);
+
+    List<String> getUserLocation();
+
+    List<String> selectLanguages();
+
+    List<LinkedHashMap> selectLanguageByUser(String login);
+
+    List<LinkedHashMap> selectNameLocation();
+
+    List<LinkedHashMap> countFirst3Followers();
+
+    List<LinkedHashMap> countFirst3Contributors();
+
+    Integer selectRankOfExperience(String login,int contribute);
+
+    Integer selectRankOfPopular(String login);
+
+    Integer selectRankOfTeamwork(String full_name);
+
+    Double getExperienceScore_Extern(int public_gists, int contributors);
+
+    Double getPopularScore_Extern(int followers);
+
+    int countAll();
+
+    Integer countGist(String login);
+
+    int countPopularScore_Extern(int sum);
+
+
+    List<LinkedHashMap> countUserCreateMonth();
+
+    List<LinkedHashMap> countOrgCreateMonth();
+
+    List<String>selectLogin();
+
+    int countContributors();
+
+    List<String>selectContributors();
+
+    Date getCreateTime(String login);
 }
---
+++ src/main/java/edu/nju/task/HttpRequest.java
@@ -0,0 +1,161 @@
+package edu.nju.task;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
+
+@SuppressWarnings("deprecation")
+public class HttpRequest {
+
+    private static final Logger log = Logger.getLogger(HttpRequest.class);
+
+    private static String token = "34dd175c177d0fee5b08339dffe7895ce8ec59d9";
+
+    public static String getGithubContentUsingHttpClient(String url) throws IOException {
+        return sendGet(token + ":x-oauth-basic@" + url);
+    }
+
+    public static String getGithubContentUsingHttpClient(String token, String url) throws IOException {
+        return sendGet(token + ":x-oauth-basic@" + url);
+    }
+
+
+    public static String getStarJson(String url,String token) throws IOException{
+        String newUrl = "https://" + token + ":x-oauth-basic@" + url;
+
+        int times = 5;
+        int responseCode = 0;
+        while (times > 0) {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(newUrl);
+
+            request.setHeader("Accept","application/vnd.github.v3.star+json");
+
+            HttpResponse response = client.execute(request);
+            responseCode = response.getStatusLine().getStatusCode();
+            if (responseCode==200) {
+                return new BasicResponseHandler().handleResponse(response);
+            }
+            times--;
+        }
+        return null;
+    }
+    public static String sendGet(String url) throws IOException {
+        String newUrl = "https://" + url;
+
+        int times = 5;
+        int responseCode = 0;
+        while (times > 0) {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(newUrl);
+            HttpResponse response = client.execute(request);
+            responseCode = response.getStatusLine().getStatusCode();
+            if (responseCode==200) {
+                return new BasicResponseHandler().handleResponse(response);
+            }
+            times--;
+        }
+        return null;
+    }
+
+//    public static String sendGet(String url) throws IOException {
+//        String newUrl = "https://" + url;
+//        HttpClient client = new DefaultHttpClient();
+//        HttpGet request = new HttpGet(newUrl);
+//        HttpResponse response = client.execute(request);
+//        String responseString = new BasicResponseHandler().handleResponse(response);
+//        return responseString;
+//    }
+
+
+    public static void downloadFile(String path, String url) throws IOException {
+        log.debug("path:" + path);
+        log.debug("url:" + url);
+        HttpClient client = null;
+        try {
+            // 创建HttpClient对象
+            client = new DefaultHttpClient();
+            // 获得HttpGet对象
+            HttpGet httpGet = new HttpGet(url);
+            int CONNECTION_TIMEOUT_MS = 3000; // Timeout in millis.
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setConnectionRequestTimeout(CONNECTION_TIMEOUT_MS)
+                    .setConnectTimeout(CONNECTION_TIMEOUT_MS)
+                    .setSocketTimeout(CONNECTION_TIMEOUT_MS)
+                    .build();
+           httpGet.setConfig(requestConfig);
+            // 发送请求获得返回结果
+            HttpResponse response = client.execute(httpGet);
+            // 如果成功
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                byte[] result = EntityUtils.toByteArray(response.getEntity());
+                BufferedOutputStream bw = null;
+                try {
+                    // 创建文件对象
+                    File f = new File(path);
+                    // 创建文件路径
+                    if (!f.getParentFile().exists())
+                        f.getParentFile().mkdirs();
+                    // 写入文件
+                    bw = new BufferedOutputStream(new FileOutputStream(path));
+                    bw.write(result);
+                } catch (Exception e) {
+                    log.error("保存文件错误,path=" + path + ",url=" + url, e);
+                } finally {
+                    try {
+                        if (bw != null)
+                            bw.close();
+                    } catch (Exception e) {
+                        log.error(
+                                "finally BufferedOutputStream shutdown close",
+                                e);
+                    }
+                }
+            }
+            // 如果失败
+            else {
+                StringBuffer errorMsg = new StringBuffer();
+                errorMsg.append("httpStatus:");
+                errorMsg.append(response.getStatusLine().getStatusCode());
+                errorMsg.append(response.getStatusLine().getReasonPhrase());
+                errorMsg.append(", Header: ");
+                Header[] headers = response.getAllHeaders();
+                for (Header header : headers) {
+                    errorMsg.append(header.getName());
+                    errorMsg.append(":");
+                    errorMsg.append(header.getValue());
+                }
+                log.error("HttpResonse Error:" + errorMsg);
+            }
+        } catch (ClientProtocolException e) {
+            log.error("下载文件保存到本地,http连接异常,path=" + path + ",url=" + url, e);
+            throw e;
+        } catch (IOException e) {
+            log.error("下载文件保存到本地,文件操作异常,path=" + path + ",url=" + url, e);
+            throw e;
+        } finally {
+            try {
+                client.getConnectionManager().shutdown();
+            } catch (Exception e) {
+                log.error("finally HttpClient shutdown error", e);
+            }
+        }
+    }
+}
