ea342ed6d1768db762ab98b9a8210ee2a9b4462d
fw14 <fw14@software.nju.edu>
2016-09-04 11:44:54 +0800

    Development:test

---
+++ src/test/edu/nju/data/Impl/RepoDaoImplTest.java
@@ -0,0 +1,154 @@
+package edu.nju.data.Impl;
+
+import org.junit.Test;
+
+/**
+ * Created by fwtgm on 2016/9/4.
+ */
+public class RepoDaoImplTest {
+    @Test
+    public void insert() throws Exception {
+
+    }
+
+    @Test
+    public void insertContribute() throws Exception {
+
+    }
+
+    @Test
+    public void insertSubscribe() throws Exception {
+
+    }
+
+    @Test
+    public void getAllFullname() throws Exception {
+
+    }
+
+    @Test
+    public void getAll() throws Exception {
+
+    }
+
+    @Test
+    public void getReposByFullName() throws Exception {
+
+    }
+
+    @Test
+    public void getContributors_Extern() throws Exception {
+
+    }
+
+    @Test
+    public void getRepository_extern() throws Exception {
+
+    }
+
+    @Test
+    public void getRepoLanguages_extern() throws Exception {
+
+    }
+
+    @Test
+    public void searchRepository() throws Exception {
+
+    }
+
+    @Test
+    public void getLanguageStatistics() throws Exception {
+
+    }
+
+    @Test
+    public void getCreatedTimeStatistics() throws Exception {
+
+    }
+
+    @Test
+    public void getCodeFrequency() throws Exception {
+
+    }
+
+    @Test
+    public void getCodeFrequency1() throws Exception {
+
+    }
+
+    @Test
+    public void enlargeViaSubscribers() throws Exception {
+
+    }
+
+    @Test
+    public void getContributors() throws Exception {
+
+    }
+
+    @Test
+    public void getCollaborators() throws Exception {
+
+    }
+
+    @Test
+    public void getHotScore() throws Exception {
+
+    }
+
+    @Test
+    public void getSizeScore() throws Exception {
+
+    }
+
+    @Test
+    public void getParticipationScore() throws Exception {
+
+    }
+
+    @Test
+    public void getContributorsScore_extern() throws Exception {
+
+    }
+
+    @Test
+    public void getPromisingScore() throws Exception {
+
+    }
+
+    @Test
+    public void getHotScore1() throws Exception {
+
+    }
+
+    @Test
+    public void getSizeScore1() throws Exception {
+
+    }
+
+    @Test
+    public void getParticipationScore1() throws Exception {
+
+    }
+
+    @Test
+    public void getPromisingScore1() throws Exception {
+
+    }
+
+    @Test
+    public void getContributorsScore() throws Exception {
+
+    }
+
+    @Test
+    public void getLastUpdate_Three() throws Exception {
+
+    }
+
+    @Test
+    public void getReposDescription() throws Exception {
+
+    }
+
+}
---
+++ src/test/edu/nju/data/Impl/RoleDaoImplTest.java
@@ -0,0 +1,35 @@
+package edu.nju.data.Impl;
+
+import edu.nju.data.dao.IRoleDao;
+import edu.nju.data.model.Skill;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by fwtgm on 2016/9/4.
+ */
+public class RoleDaoImplTest {
+    @Resource
+    private IRoleDao dao;
+    @Test
+    public void selectRole() throws Exception {
+
+    }
+
+    @Test
+    public void showSkills() throws Exception {
+        String name="Web Developer";
+        List<Skill> list=dao.showSkills(name);
+        for(Skill s:list){
+            System.out.println(s.getName());
+        }
+    }
+
+    @Test
+    public void levelUp() throws Exception {
+
+    }
+
+}
