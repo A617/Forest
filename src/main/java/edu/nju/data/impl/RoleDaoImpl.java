8762c79fed65d985c6cd5f5a78f9dc6fb3be2b79
phoebegl <phoebegl74@163.com>
2016-09-05 08:55:02 +0800

    删除一些东西

--- src/main/java/edu/nju/data/impl/RoleDaoImpl.java
+++ src/main/java/edu/nju/data/impl/RoleDaoImpl.java
@@ -18,8 +18,6 @@ import java.util.List;
 /**
  * Created by fwtgm on 2016/9/1.
  */
-@RunWith(SpringJUnit4ClassRunner.class)        //表示继承了SpringJUnit4ClassRunner类
-@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
 
 public class RoleDaoImpl implements IRoleDao{
     @Resource
