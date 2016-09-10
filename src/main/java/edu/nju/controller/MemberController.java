f7e17893b0e43771ca7510e81191068533110358
oraisdy <569450503@qq.com>
2016-09-11 00:47:24 +0800

    明天再说

--- src/main/java/edu/nju/controller/MemberController.java
+++ src/main/java/edu/nju/controller/MemberController.java
@@ -5,10 +5,8 @@ import edu.nju.service.vo.SignedInUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.servlet.http.HttpSession;
@@ -60,5 +58,16 @@ public class MemberController {
             sessionStatus.setComplete();
         }
         return "redirect:" + formerUrl;
+    }
+
+    @RequestMapping("/achievements/{username}")
+    String showAchievements(@PathVariable("username") String username, Model model){
+        return "achievements";
+    }
+
+    @RequestMapping("/achievements")
+    String showMyAchievements(HttpSession session){
+        SignedInUser user = LoginHelper.getSignInUser(session);
+        return "redirect:/achievements/"+user.getUsername();
     }
 }
